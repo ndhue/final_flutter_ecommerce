1c34f82267caaa222be299794240b90c8412a84f
Bùi Thị Phương Anh <522000004@student.tdt.edu.vn>
2025-03-15 21:05:06 +0700

    conflix

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget buiild(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Demo',
